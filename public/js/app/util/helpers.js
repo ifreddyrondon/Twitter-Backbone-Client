398ac6103f8b57b574be5f3262c454341cbadabd
spantons <spantons@gmail.com>
2014-07-22 16:14:27 -0430

    delete css rules, profile fixed, highlighted links
    profile class fixed
    delete unused css rules
    text of the tweet with highlighted links

--- public/js/app/util/helpers.js
+++ public/js/app/util/helpers.js
@@ -2,15 +2,28 @@ $(function(){
 
 	Handlebars.registerHelper('format', function(str){
 		if (str) {
+
 			// highliht the @part
 			str = str.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
                 var username = u.replace("@","");
                 return '<a href="#" data-user="' + username +'" class="profile">@'+username+'</a>';
             });
+
+			// highliht email
+			str = str.replace(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi, function(u) {
+                return '<a href="mailto:'+ u +'" target="_blank">'+u+'</a>';
+            });
+
+			// highliht url
+            str = str.replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig, function(u) {
+                return '<a href="'+ u +'" target="_blank">'+u+'</a>';
+            });
+
            return new Handlebars.SafeString(str);
 		} else {
 			return str;
 		}
 	});
+
 
 });
